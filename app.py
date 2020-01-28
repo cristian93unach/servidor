f2e8c0b5d57f5c7cbb8a1f56f42d0ca6677c74b3
cristian93unach <59587432+cristian93unach@users.noreply.github.com>
2020-01-28 10:17:39 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -23,17 +23,20 @@ class myHandler(BaseHTTPRequestHandler):
 	
 	#Handler for the GET requests
 	def do_GET(self):
-		path=self.path
-		print(self.path)
+		print(self.path.split('/')[-1])
+		nombre=self.path.split('/')[-1]
+		datos=''
 		if self.path=="/":  #127.0.0.1:5000/
-			self.path="/index.html" #127.0.0.1:5000/index.html
+			nombre="index.html" #127.0.0.1:5000/index.html
 		try:
 			#Check the file extension required and
-			#set the right mime type
-
+			#set the right mime typeS
 			sendReply = False
-			if self.path.endswith(".html"):
+			if nombre.endswith(".html"):
 				mimetype='text/html'
+				f=open(nombre)
+				datos=f.read()
+				f.close()
 				sendReply = True
 			if self.path.endswith(".jpg"):
 				mimetype='image/jpg'
@@ -56,9 +59,9 @@ class myHandler(BaseHTTPRequestHandler):
 				self.end_headers()
 				
 				try:
-					self.wfile.write('hola mundo')
+					self.wfile.write(datos)##python2
 				except:
-					self.wfile.write(bytes('hola mundo', 'UTF-8'))
+					self.wfile.write(bytes(datos, 'UTF-8'))
 				
 			return
 
